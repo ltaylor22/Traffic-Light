93d4b0397086f8dacdabf7a2c2da9a2cac3f3db4
ltaylor22 <luisa.taylor22@gmail.com>
2023-03-09 01:33:43 +0000

    ltaylor

--- src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-export const Footer = () => {
-  return (
-    <p className="text-center">
-      Made by <a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-      love!
-    </p>
-  );
-};
